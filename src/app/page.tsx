033c505bca88881cdb25f38c3e965b50fdf749b8
wgomes-brain <wander@braingenai.ca>
2025-07-10 00:47:05 -0600

    No errors now, but no welcome page either

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
+//import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Link href="/api/auth/login">
+      <a href="/auth/login">
         <Button>Login</Button>
-      </Link>
+      </a>
     </main>
   );
 }
--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,16 +1,26 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
 import { auth0 } from '../../lib/auth0'
 import { Button } from '@/components/ui/button';
-import Link from 'next/link';
+//import Link from 'next/link';
 
 export default async function Welcome() {
   const session = await auth0.getSession();
 
+  if (!session) {
+    return (
+      <main>
+        <a href="/auth/login?screen_hint=signup">Sign up</a>
+        <a href="/auth/login">Log in</a>
+      </main>
+    );
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="mb-8 text-4xl font-bold">Welcome, {session?.user.name}</h1>
-      <Link href="/api/auth/logout">
+      <a href="/api/auth/logout">
         <Button>Logout</Button>
-      </Link>
+      </a>
     </main>
   );
 }
