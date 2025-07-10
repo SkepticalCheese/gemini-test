fa165177675fcd7298e8b379fcdb34b8f1375950
wgomes-brain <wander@braingenai.ca>
2025-07-10 01:00:19 -0600

    Welcome page is working now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 import { Button } from "@/components/ui/button";
+import { auth0 } from '../lib/auth0'
 //import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth0.getSession();
+  
+  if (session) {
+    return (
+      <main>
+        <a href="/welcome">Welcome Page</a>
+      </main>
+    );
+  }
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <a href="/auth/login">
--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -18,7 +18,7 @@ export default async function Welcome() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="mb-8 text-4xl font-bold">Welcome, {session?.user.name}</h1>
-      <a href="/api/auth/logout">
+      <a href="/auth/logout">
         <Button>Logout</Button>
       </a>
     </main>
