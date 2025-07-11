de425e6a217e53256a37bf1e81143c967260e077
Wander <wapabrgo@gmail.com>
2025-07-10 23:05:10 -0600

    Finally this is logging out when user is not logged in

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,28 @@
-import type { NextRequest } from "next/server";
-
-import { auth0 } from "@/lib/auth0";
+import { NextResponse, NextRequest } from "next/server";
+import { auth0 } from "./lib/auth0"
 
 export async function middleware(request: NextRequest) {
-  return await auth0.middleware(request);
+    const authRes = await auth0.middleware(request);
+
+    // authentication routes — let the middleware handle it
+    if (request.nextUrl.pathname.startsWith("/auth")) {
+        return authRes;
+    }
+
+    // // public routes — no need to check for session
+    // if (request.nextUrl.pathname === ("/")) {
+    //     return authRes;
+    // }
+
+    const { origin } = new URL(request.url)
+    const session = await auth0.getSession(request)
+
+    // user does not have a session — redirect to login
+    if (!session) {
+        return NextResponse.redirect(`${origin}/auth/login`)
+    }
+
+    return authRes
 }
 
 export const config = {
@@ -14,7 +33,8 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
      * - $ (homepage)
+     * - auth routes
      */
-    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|$).*)"
+    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"
   ]
 };
