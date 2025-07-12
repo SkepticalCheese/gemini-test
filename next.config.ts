c6ea62f2b5ed2438a40c007be605e292aad2acab
Wander <wapabrgo@gmail.com>
2025-07-12 01:41:04 -0600

    Updated next config to hide dev tools on production

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = 
+  process.env.NODE_ENV === 'production'
+    ? {devIndicators: false} 
+    : {}
+;
 
 export default nextConfig;
